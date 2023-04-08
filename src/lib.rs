66ea7f2dc9420e739c91848e3e4ec6ee15c2ddc1
nonowar7 <sabalanoam77@gmail.com>
2023-04-08 14:18:36 +0300

    README

--- src/lib.rs
+++ src/lib.rs
@@ -185,9 +185,16 @@
 //! 
 //! ```
 //!
+//! ```
+//! 
 //! # References
+//! I used the crates: [id-tree](https://crates.io/crates/id_tree), [plotters](https://crates.io/crates/plotters).
+//! I made two demo examples with [spaCy](https://spacy.io/) and [Berkeley Neural Parser](https://pypi.org/project/benepar/), slightly changed POS names.
 //! 
+//! # License
+//! Under MIT license.
 //! 
+//! ```
 //! 
 
 mod config;
