c2c8e840bb78acab4cf54fdca98aaca94dd8e9cb
nonowar7 <sabalanoam77@gmail.com>
2023-04-09 11:00:41 +0300

    examples

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! 
 //! # Overview
 //! 
-//! The code uses both the [id-tree](https://crates.io/crates/id_tree) crate and the [plotters](https://crates.io/crates/plotters) crate. While primarily written with linguistic syntax in mind, it can serve other inputs, such as mathematical expressions etc. The API first transforms the input to an internal conll / tree, then plots the structure recursively. It is mostly suitable for short parsed sequences of up to 15-20 tokens. The program is a simple drawing program, plots strings that are already parsed. This is not a parser! I wrote this in order to get familiar with Rust and decided to upload it if it can help others.
+//! The code uses both the [id-tree](https://crates.io/crates/id_tree) crate and the [plotters](https://crates.io/crates/plotters) crate. While primarily written with linguistic syntax in mind, it can serve other inputs, such as mathematical expressions etc. The program first transforms the input to a conll / tree, then plots the structure recursively. It is mostly suitable for short parsed sequences of up to 15-20 tokens. The program is a simple drawing program, plots strings that are already parsed. This is not a parser! I wrote this in order to get familiar with Rust and decided to upload it if it can help others.
 //! 
 //! # Input-Output
 //! 
