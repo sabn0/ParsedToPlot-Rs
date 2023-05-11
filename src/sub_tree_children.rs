596f5d97343899c7b9dddc71b9597e7f35a707fa
nonowar7 <sabalanoam77@gmail.com>
2023-05-12 00:30:41 +0300

    comments, imports, visibility and tests on sub_tree_children.rs

--- src/sub_tree_children.rs
+++ src/sub_tree_children.rs
@@ -1,46 +1,40 @@
 
 
-/// A trait to get an iterator over the sub-tree-children-ids a node has
-/// using the -id-tree children impl. In a private module.
+// A trait to get an iterator over the sub-tree-children-ids a node has
+// using the -id-tree children impl. Available within crate.
 pub mod sub_tree_children {
 
     use std::collections::HashMap;
-    use std::fmt::Error;
-    use id_tree::{NodeIdError, Tree, NodeId};
+    use std::error::Error;
+    use id_tree::{Tree, NodeId};
 
-    pub(crate) trait SubChildren {
-        fn is_leaf(&self, node_id: &NodeId) -> Result<bool, Error>;
-        fn get_sub_children(&mut self, as_leaves: bool) -> Result<HashMap<NodeId, usize>, NodeIdError>;
+    pub(in crate) trait SubChildren {
+        fn is_leaf(&self, node_id: &NodeId) -> Result<bool, Box<dyn Error>>;
+        fn get_sub_children(&mut self, as_leaves: bool) -> Result<HashMap<NodeId, usize>, Box<dyn Error>>;
     }
 
     impl SubChildren for Tree<String> {
 
-        /// 
-        /// A method that checks if a node is a leaf (i.e has no children)
-        /// Input is a &NodeId. Returns a true result if leaf, or false result otherwise.
-        /// 
-        /// This method is used via get_sub_children method.
-        /// 
-        fn is_leaf(&self, node_id: &NodeId) -> Result<bool, Error> {
-            
-            let children_node_ids  = match self.children_ids(node_id) {
-                Ok(sub_children_ids) => sub_children_ids,
-                Err(e) => panic!("error happened in children extraction: {}", e)
-            };
+        // 
+        // A method that checks if a node is a leaf (i.e has no children)
+        // Input is a &NodeId. Returns a true result if leaf, or false result otherwise.
+        // 
+        // This method is used via get_sub_children method. 
+        // 
+        fn is_leaf(&self, node_id: &NodeId) -> Result<bool, Box<dyn Error>> {
+            let children_node_ids  = self.children_ids(node_id)?;
             return Ok(children_node_ids.peekable().peek().is_none())
         }
 
-        ///
-        /// A method that given a tree returns a mapping between node id and number of children in 
-        /// each node's sub tree, from node to leaves. The method receives a boolean parameter as_leaves.
-        /// 
-        /// The method treats leaves as having 1 child (themselves).
-        /// 
-        /// When as_leaves is true : counts only the leaves in each node's sub tree.
-        /// When as_leaves is false: counts all the nodes in some node's sub tree, from node to leaves,
-        /// including the node itself.
-        /// 
-        fn get_sub_children(&mut self, as_leaves: bool) -> Result<HashMap<NodeId, usize>, NodeIdError> {
+        //
+        // A method that given a tree returns a mapping between node_id and number of children in 
+        // the node's sub tree, from node to leaves. The method receives a boolean parameter as_leaves.
+        // The method treats leaves as having 1 child (themselves).
+        // 
+        // When as_leaves is true : counts only the leaves in each node's sub tree.
+        // When as_leaves is false: counts all the nodes in some node's sub tree, from node to leaves, including the node itself.
+        // 
+        fn get_sub_children(&mut self, as_leaves: bool) -> Result<HashMap<NodeId, usize>, Box<dyn Error>> {
 
             let root_id = match self.root_node_id() {
                 Some(root_id) => root_id,
@@ -49,12 +43,11 @@ pub mod sub_tree_children {
 
             let account_for_node = !as_leaves as usize;
             let mut map: HashMap<NodeId, usize> = HashMap::new();
-            let post_order_iter = self.traverse_post_order_ids(root_id).unwrap();
+            let post_order_iter = self.traverse_post_order_ids(root_id)?;
             for node_id in post_order_iter {
 
                 // this is a post order traversal, so I add the leaves to the map first,
                 // then I add them to their parents counts in O(1) time.
-
                 let node_id_copy = node_id.clone();
                 if self.is_leaf(&node_id).unwrap() {
                     map.insert(node_id_copy, 1);
@@ -90,20 +83,15 @@ mod tests {
 
         let mut sequence = String::from(example);
         let mut string2tree: String2Tree = String2StructureBuilder::new();
-
-        let _result = match string2tree.build(&mut sequence) {
-            Ok(_result) => {},
-            Err(_e) => panic!("error building tree")
-        };
-
+        string2tree.build(&mut sequence).unwrap();
+        
         let mut tree = string2tree.get_structure();
         let n_sub_leaves = match tree.get_sub_children(as_leaves) {
             Ok(n_sub_leaves) => n_sub_leaves,
-            Err(_e) => panic!("could not calculate number of sub children")
+            Err(e) => panic!("{}", e)
         };
 
         let mut iter: PreOrderTraversalIds<String> = tree.traverse_pre_order_ids(tree.root_node_id().unwrap()).unwrap();
-        
         while let Some(node_id) = iter.next() {
 
             let node = tree.get(&node_id).unwrap().data().as_str();
@@ -122,9 +110,9 @@ mod tests {
     #[test]
     fn as_leaves_validation() {
 
-        let example = "(S (0 (1) (2 (3) (4))))";
+        let example = "(0 (1 (2) (3 (4) (5))))";
         let golden = HashMap::from([
-            ("S", 3), ("0", 3), ("1", 1), ("2", 2), ("3", 1), ("4", 1)
+            ("0", 3), ("1", 3), ("2", 1), ("3", 2), ("4", 1), ("5", 1)
         ]);
 
         sub_children_template(example, golden, true);
@@ -134,9 +122,9 @@ mod tests {
     #[test]
     fn not_as_leaves_validation_1() {
 
-        let example = "(S (0 (1) (2 (3) (4))))";
+        let example = "(0 (1 (2) (3 (4) (5))))";
         let golden = HashMap::from([
-            ("S", 6), ("0", 5), ("1", 1), ("2", 3), ("3", 1), ("4", 1)
+            ("0", 6), ("1", 5), ("2", 1), ("3", 3), ("4", 1), ("5", 1)
         ]);
 
         sub_children_template(example, golden, false);
@@ -146,10 +134,9 @@ mod tests {
     #[test]
     fn not_as_leaves_validation_2() {
 
-        let example = "(S (0 (1)) (2 (3) (4)))";
-        //let example = "(S (0 (1) (2 (3) (4))))";
+        let example = "(0 (1 (2)) (3 (4) (5)))";
         let golden = HashMap::from([
-            ("S", 6), ("0", 2), ("1", 1), ("2", 3), ("3", 1), ("4", 1)
+            ("0", 6), ("1", 2), ("2", 1), ("3", 3), ("4", 1), ("5", 1)
         ]);
 
         sub_children_template(example, golden, false);
@@ -160,10 +147,8 @@ mod tests {
 
         let mut string2tree: String2Tree = String2StructureBuilder::new();
         let mut constituency = String::from("(S (NP (det The) (N people)) (VP (V watch) (NP (det the) (N game))))");
-        let _res = match string2tree.build(&mut constituency) {
-                Ok(_res) => Ok(_res),
-            Err(e) => Err(e)
-        };
+        string2tree.build(&mut constituency).unwrap();
+
         let tree = string2tree.get_structure();
         let root_node_id: &NodeId = tree.root_node_id().unwrap();
         let is_leaf_result: bool = tree.is_leaf(root_node_id).unwrap();
